0748f295ba61150961a5288837f6a7f95bb6225c
Görföl Botond <botond.gorfol@gmail.com>
2017-12-07 14:32:05 +0100

    backend recreated in .net 2.0

---
+++ InstallerBackend/InstallerBackend/Controller/RestController.cs
@@ -0,0 +1,37 @@
+﻿using InstallerBackend.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstallerBackend.Controllers
+{
+    [Route("")]
+    public class RestController : Controller
+    {
+        TechnologyList technologyList;
+
+        public RestController(TechnologyList technologyList)
+        {
+            this.technologyList = technologyList;
+        }
+
+        [HttpGet]
+        [Route("/query")]
+        public IActionResult ListTechnology([FromQuery] string focus)
+        {
+            if (focus == "c#")
+            {
+                return Json(new { result = "ok", data = technologyList.FilterFocus(focus) });
+            }
+            else if (focus == "java")
+            {
+                return Json(new { result = "ok", data = technologyList.FilterFocus(focus) });
+            }
+            else if (focus == "javascript")
+            {
+                return Json(new { result = "ok", data = technologyList.FilterFocus(focus) });
+            }
+            else
+
+                return Json(new { result = "error", error = "Wrong query!" });
+        }
+    }
+}
