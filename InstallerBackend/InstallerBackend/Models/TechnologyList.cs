fb95d593b8c91857740bed182c55b9252a2dc492
Görföl Botond <botond.gorfol@gmail.com>
2017-12-08 10:54:32 +0100

    changed links is the list

--- InstallerBackend/InstallerBackend/Models/TechnologyList.cs
+++ InstallerBackend/InstallerBackend/Models/TechnologyList.cs
@@ -8,7 +8,7 @@ namespace InstallerBackend.Model
         public List<Technology> TechList { get; set; } = new List<Technology>
         {
             new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/", "1.1.7", "installstring"),
-            new Technology("java", "NetBeans", "https://netbeans.org/downloads/", "1.1.8", "installstring"),
+            new Technology("java", "IntelliJ IDEA", "https://www.jetbrains.com/idea/download/#section=windows", "1.1.8", "installstring"),
             new Technology("javascript", "NotePad", "https://www.greenfoxacademy.com/", "1.1.9", "installstring")
         };
 
