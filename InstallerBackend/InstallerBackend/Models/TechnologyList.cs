0e6814765388b0bfab07e22c8fc7a248ee9da537
Görföl Botond <botond.gorfol@gmail.com>
2018-01-04 13:11:38 +0100

    change filterfocus

--- InstallerBackend/InstallerBackend/Models/TechnologyList.cs
+++ InstallerBackend/InstallerBackend/Models/TechnologyList.cs
@@ -8,14 +8,18 @@ namespace InstallerBackend.Model
         public List<Technology> TechList { get; set; } = new List<Technology>
         {
             new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/", "1.1.7", "installstring"),
+            new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/", "1.1.7", "installstring"),
+            new Technology("java", "IntelliJ IDEA", "https://www.jetbrains.com/idea/download/#section=windows", "1.1.8", "installstring"),
             new Technology("java", "IntelliJ IDEA", "https://www.jetbrains.com/idea/download/#section=windows", "1.1.8", "installstring"),
             new Technology("javascript", "NotePad", "https://www.greenfoxacademy.com/", "1.1.9", "installstring"),
+            new Technology("javascript", "NotePad", "https://www.greenfoxacademy.com/", "1.1.9", "installstring"),
+            new Technology("hardware", "GCC/c++", "https://www.greenfoxacademy.com/", "1.1.9", "installstring"),
             new Technology("hardware", "GCC/c++", "https://www.greenfoxacademy.com/", "1.1.9", "installstring")
         };
 
-        public Technology FilterFocus(string focus)
+        public List<Technology> FilterFocus(string focus)
         {
-            return TechList.FirstOrDefault(technology => technology.Focus == focus);
+            return TechList.FindAll(technology => technology.Focus == focus);
         }
     }
 }
