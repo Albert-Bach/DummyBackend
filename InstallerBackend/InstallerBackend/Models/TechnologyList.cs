31630f21839239b7fde30887852c5100691379ed
Görföl Botond <botond.gorfol@gmail.com>
2018-01-05 15:36:48 +0100

    feat: modify c# focus

--- InstallerBackend/InstallerBackend/Models/TechnologyList.cs
+++ InstallerBackend/InstallerBackend/Models/TechnologyList.cs
@@ -7,11 +7,11 @@ namespace InstallerBackend.Model
     {
         public List<Technology> TechList { get; set; } = new List<Technology>
         {
-            new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/", "1.1.7", "installstring"),
-            new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/", "1.1.7", "installstring"),
+            new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/?sku=Community&rel=15#", "15.5.27130.2010", "installstring"),
+            new Technology("c#", "Python", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "3.6.4", "installstring"),
             new Technology("java", "IntelliJ IDEA", "https://www.jetbrains.com/idea/download/#section=windows", "1.1.8", "installstring"),
             new Technology("java", "IntelliJ IDEA", "https://www.jetbrains.com/idea/download/#section=windows", "1.1.8", "installstring"),
-            new Technology("javascript", "Python", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "1.1.9", "installstring"),
+            new Technology("javascript", "Python", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "3.6.4", "installstring"),
             new Technology("javascript", "NotePad", "https://www.greenfoxacademy.com/", "1.1.9", "installstring"),
             new Technology("hardware", "GCC/c++", "https://www.greenfoxacademy.com/", "1.1.9", "installstring"),
             new Technology("hardware", "GCC/c++", "https://www.greenfoxacademy.com/", "1.1.9", "installstring")
