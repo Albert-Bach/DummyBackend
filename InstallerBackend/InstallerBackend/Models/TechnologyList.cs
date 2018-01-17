0bb76a6148ee5a33b7a96f3aafb2a864df45ba4e
Görföl Botond <botond.gorfol@gmail.com>
2018-01-17 11:54:20 +0100

    set versionnumbers null

--- InstallerBackend/InstallerBackend/Models/TechnologyList.cs
+++ InstallerBackend/InstallerBackend/Models/TechnologyList.cs
@@ -8,14 +8,14 @@ namespace InstallerBackend.Model
 
         public List<Technology> TechList { get; set; } = new List<Technology>
         {
-            new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/?sku=Community&rel=15#", "vs.exe" , "15.5.27130.2010", "installstring", null),
-            new Technology("c#", "Python", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "python.exe" , "3.6.6032.0", "/s InstallAllUsers=1 PrependPath=1", null),
-            new Technology("java", "IntelliJ IDEA", "https://www.jetbrains.com/idea/download/#section=windows", "ii.exe" , "1.1.8", "installstring", null),
-            new Technology("java", "javaSDK", "http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_windows-x64_bin.exe", "jdk.exe" , "1.1.8", $"/s ADDLOCAL=\"ToolsFeature,SourceFeature,PublicjreFeature\"", null),
-            new Technology("javascript", "Python", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "python.exe" , "3.6.6032.0", "/quiet InstallAllUsers=1 PrependPath=1", null),
-            new Technology("javascript", "NotePad", "https://www.greenfoxacademy.com/", "notepad.exe" , "1.1.9", "installstring", null),
-            new Technology("c++", "GCC/c++", "https://www.greenfoxacademy.com/", "gc.exe" , "1.1.9", "installstring", null),
-            new Technology("c++", "GCC/c++", "https://www.greenfoxacademy.com/", "gc.exe", "1.1.9", "installstring", null)
+            new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/?sku=Community&rel=15#", "vs.exe" , null, "installstring", null),
+            new Technology("c#", "Python", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "python.exe" , null, "/s InstallAllUsers=1 PrependPath=1", null),
+            new Technology("java", "IntelliJ IDEA", "https://www.jetbrains.com/idea/download/#section=windows", "ii.exe" , null, "installstring", null),
+            new Technology("java", "javaSDK", "http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_windows-x64_bin.exe", "jdk.exe" , null, $"/s ADDLOCAL=\"ToolsFeature,SourceFeature,PublicjreFeature\"", null),
+            new Technology("javascript", "Python", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "python.exe" , null, "/quiet InstallAllUsers=1 PrependPath=1", null),
+            new Technology("javascript", "NotePad", "https://www.greenfoxacademy.com/", "notepad.exe" , null, "installstring", null),
+            new Technology("c++", "GCC/c++", "https://www.greenfoxacademy.com/", "gc.exe" , null, "installstring", null),
+            new Technology("c++", "GCC/c++", "https://www.greenfoxacademy.com/", "gc.exe", null, "installstring", null)
         };
 
         public List<Technology> FilterFocus(string focus)
