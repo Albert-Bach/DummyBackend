4ea1c99873fca44f02ab681a0432c0ff40e52ba6
Görföl Botond <botond.gorfol@gmail.com>
2018-01-17 09:56:47 +0100

    fix constructor

--- InstallerBackend/InstallerBackend/Models/Technology.cs
+++ InstallerBackend/InstallerBackend/Models/Technology.cs
@@ -16,7 +16,7 @@
             DownloadUrl = downloadUrl;
             VersionNumber = versionNumber;
             InstallString = installString;
-            UninstallString = installString;
+            UninstallString = uninstallString;
         }
     }
 }
