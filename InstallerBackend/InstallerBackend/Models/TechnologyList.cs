43da9f0afa2c8efa44dd340ca834131fb5daba39
Görföl Botond <botond.gorfol@gmail.com>
2018-01-19 09:06:46 +0100

    change java focus

--- InstallerBackend/InstallerBackend/Models/TechnologyList.cs
+++ InstallerBackend/InstallerBackend/Models/TechnologyList.cs
@@ -10,7 +10,7 @@ namespace InstallerBackend.Model
         {
             new Technology("c#", "Visual Studio", "https://www.visualstudio.com/thank-you-downloading-visual-studio/?sku=Community&rel=15#", "vs.exe" , "Not Installed", "installstring", null),
             new Technology("c#", "Python Launcher", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "python.exe" , "Not Installed", "/s InstallAllUsers=1 PrependPath=1", null),
-            new Technology("java", "IntelliJ IDEA", "https://www.jetbrains.com/idea/download/#section=windows", "ii.exe" , "Not Installed", "installstring", null),
+            new Technology("java", "Python Launcher", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "python.exe" , "Not Installed", "/s InstallAllUsers=1 PrependPath=1", null),
             new Technology("java", "javaSDK", "http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_windows-x64_bin.exe", "jdk.exe" , "Not Installed", $"/s ADDLOCAL=\"ToolsFeature,SourceFeature,PublicjreFeature\"", null),
             new Technology("javascript", "Python Launcher", "https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe", "python.exe" , "Not Installed", "/quiet InstallAllUsers=1 PrependPath=1", null),
             new Technology("javascript", "NotePad", "https://www.greenfoxacademy.com/", "notepad.exe" , "Not Installed", "installstring", null),
